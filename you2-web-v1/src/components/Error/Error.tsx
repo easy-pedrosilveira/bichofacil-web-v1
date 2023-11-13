ffc6e33a0c6cfe80ab56ac9244b0214707304bc8
easy-lucasgabriel <122984290+easy-lucasgabriel@users.noreply.github.com>
2023-11-13 14:02:28 -0300

    Delete you2-web-v1 directory

--- you2-web-v1/src/components/Error/Error.tsx
+++
@@ -1,8 +0,0 @@
-
-export const Error = () => {
-  return (
-    <div>
-      error
-    </div>
-  );
-};
--- you2-web-v1/src/components/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from './Text';
-export * from './Input';
-export * from './Navbar';
-export * from './Error';
