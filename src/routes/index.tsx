a14a6f755bdb3e14924fbf95960abcdcb90227c3
easy-lucasgabriel <lucas.gabriel@easychange.com.br>
2023-11-16 11:17:42 -0300

    iniciando o layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from 'pages';
-import { Header, Error } from 'components'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "pages";
+import { Header, Error } from "components";
+import styles from "../styles/Routes.module.css";
 
 export const Rotas = () => {
-  return <main>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
-  </main>
-}
+  return (
+    <main className={styles.main}>
+      <BrowserRouter>
+        <div className={styles.header}><Header /></div>
+        <div className={styles.menu}></div>
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </div>
+        <div className={styles.footer}>
+
+        </div>
+      </BrowserRouter>
+    </main>
+  );
+};
