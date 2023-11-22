87078ad781c14eef5346ff9835379b40d7925f23
Lucas Gabriel <lucas.gabriel@easychange.com.br>
2023-11-22 13:41:56 -0300

    criando o modal de cadastro já funcional

--- src/components/Modals/Register/Register.tsx
+++ src/components/Modals/Register/Register.tsx
@@ -3,28 +3,224 @@ import { useState } from "react";
 import useAuthContext from "data/hooks/useAuthContext";
 import { useForm } from "react-hook-form";
 import InputMask from "react-input-mask";
+import { motion } from "framer-motion";
+import { nameRegex, emailRegex, phoneRegex, passwordRegex } from "utils";
+import { cpf } from "cpf-cnpj-validator";
+import { apiAuth } from "providers";
 
 export const Register = () => {
   const { setModalRegister } = useAuthContext();
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { handleSubmit } = useForm();
+  const [birthdate, setBirthdate] = useState<string>("");
+  const [firstname, setFirstName] = useState<string>("");
+  const [lastname, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmpassword, setConfirmPassword] = useState<string>("");
+  const [cpfvalue, setCpf] = useState<string>("");
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+  const onSubmit = async (data: any) => {
+    const nameValidation = validateName();
+    const ageValidation = validateAge();
+    const emailValidation = validateEmail();
+    const phoneValidation = validatePhone();
+    const cpfValidation = validateCpf();
+    const passwordValidation = validatePassword();
+    const confirmPasswordValidation = validatePasswordEqual();
 
-  const validateAge = (value: string) => {
-    const birthdate = new Date(value);
-    const today = new Date();
-    const age = today.getFullYear() - birthdate.getFullYear();
+    if (
+      nameValidation &&
+      ageValidation &&
+      emailValidation &&
+      phoneValidation &&
+      cpfValidation &&
+      passwordValidation &&
+      confirmPasswordValidation
+    ) {
 
-    if (age < 18) {
-      return "Você deve ter pelo menos 18 anos.";
+      const formData = {
+        firstname,
+        lastname,
+        birthdate,
+        cpf: cpfvalue,
+        phone,
+        email,
+        password,
+      };
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      apiAuth
+        .post("/user/register/", formData, config)
+        .then((response) => {
+          alert("Cadastro Realizado");
+        })
+        .catch((error) => {
+          if (error.response) {
+            const errorMessage = error.response.data.detail;
+            alert(errorMessage);
+          } else {
+            alert("Erro desconhecido");
+          }
+          console.log(error);
+        });
     }
-
-    return true;
   };
 
-  console.log(errors)
+  const validateName = () => {
+    if (!firstname.match(nameRegex)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        firstname: "O nome precisa conter apenas letras e espaços",
+      }));
+      return false;
+    } else if (!lastname.match(nameRegex)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        lastname: "O sobrenome precisa conter apenas letras e espaços",
+      }));
+      return false;
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        firstname: "",
+        lastname: "",
+      }));
+      return true;
+    }
+  };
+
+  const validateAge = () => {
+    const MinAge = 18;
+    const MaxAge = 95;
+
+    const parts = birthdate.split("/");
+    if (parts.length === 3) {
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10);
+      const year = parseInt(parts[2], 10);
+
+      const currentDate = new Date();
+      const minDate = new Date(
+        currentDate.getFullYear() - MinAge,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      );
+      const maxDate = new Date(
+        currentDate.getFullYear() - MaxAge,
+        currentDate.getMonth(),
+        currentDate.getDate()
+      );
+
+      const inputDate = new Date(year, month - 1, day);
+
+      if (inputDate >= minDate) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          birthdate: `Você deve ter no mínimo ${MinAge} anos de idade.`,
+        }));
+        return false;
+      } else if (inputDate <= maxDate) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          birthdate: `Você deve ter no máximo ${MaxAge} anos de idade.`,
+        }));
+        return false;
+      } else {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          birthdate: "",
+        }));
+        return true;
+      }
+    }
+  };
+
+  const validateEmail = () => {
+    if (!emailRegex.test(email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Por favor insira um email válido!",
+      }));
+      return false;
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "",
+      }));
+      return true;
+    }
+  };
+
+  const validatePhone = () => {
+    if (!phoneRegex.test(phone)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        phone: "Por favor insira um telefone válido!",
+      }));
+      return false;
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        phone: "",
+      }));
+      return true;
+    }
+  };
+
+  const validateCpf = () => {
+    if (!cpf.isValid(cpfvalue)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        cpf: "Por favor insira um cpf válido!",
+      }));
+      return false;
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        cpf: "",
+      }));
+      return true;
+    }
+  };
+
+  const validatePassword = () => {
+    if (!passwordRegex.test(password)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password:
+          "A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.",
+      }));
+      return false;
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "",
+      }));
+      return true;
+    }
+  };
+
+  const validatePasswordEqual = () => {
+    if (password !== confirmpassword) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        passwordConfirmation: "As senhas não coincidem",
+      }));
+      return false;
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        passwordConfirmation: "",
+      }));
+      return true;
+    }
+  };
 
   return (
     <main
@@ -37,7 +233,16 @@ export const Register = () => {
         }
       }}
     >
-      <div className={styles.modal}>
+      <motion.div
+        className={styles.modal}
+        initial={{ y: 900 }}
+        animate={{ y: 0 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
         <div className={styles.content}>
           <div className={styles.introduction}>Faça seu cadastro</div>
           <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
@@ -47,8 +252,17 @@ export const Register = () => {
                 className={styles.input}
                 placeholder="Insira seu primeiro nome"
                 required
-                {...register("firstname")}
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                  setErrors({
+                    ...errors,
+                    firstname: "",
+                  });
+                }}
               />
+              {errors.firstname && (
+                <p style={{ color: "red" }}>{errors.firstname}</p>
+              )}
             </div>
             <div className={styles.info}>
               <input
@@ -56,18 +270,35 @@ export const Register = () => {
                 className={styles.input}
                 placeholder="Insira seu sobrenome"
                 required
-                {...register("lastname")}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                  setErrors({
+                    ...errors,
+                    lastname: "",
+                  });
+                }}
               />
+              {errors.lastname && (
+                <p style={{ color: "red" }}>{errors.lastname}</p>
+              )}
             </div>
             <div className={styles.info}>
               <InputMask
-                mask="99/99/9999"
-                type="text"
                 className={styles.input}
-                placeholder="Insira sua data de nascimento"
-                required
-                {...register("birthdate", { required: "Este campo é obrigatório.", validate: validateAge })}
+                mask={"99/99/9999"}
+                type="text"
+                placeholder="dd/mm/aaaa"
+                onChange={(e) => {
+                  setBirthdate(e.target.value);
+                  setErrors({
+                    ...errors,
+                    birthdate: "",
+                  });
+                }}
               />
+              {errors.birthdate && (
+                <p style={{ color: "red" }}>{errors.birthdate}</p>
+              )}
             </div>
             <div className={styles.info}>
               <InputMask
@@ -76,8 +307,15 @@ export const Register = () => {
                 className={styles.input}
                 placeholder="Insira seu cpf"
                 required
-                {...register("cpf")}
+                onChange={(e) => {
+                  setCpf(e.target.value);
+                  setErrors({
+                    ...errors,
+                    cpf: "",
+                  });
+                }}
               />
+              {errors.cpf && <p style={{ color: "red" }}>{errors.cpf}</p>}
             </div>
             <div className={styles.info}>
               <InputMask
@@ -86,8 +324,15 @@ export const Register = () => {
                 placeholder="Insira seu telefone"
                 required
                 mask="(99)99999-9999"
-                {...register("phonenumber")}
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                  setErrors({
+                    ...errors,
+                    phone: "",
+                  });
+                }}
               />
+              {errors.phone && <p style={{ color: "red" }}>{errors.phone}</p>}
             </div>
             <div className={styles.info}>
               <input
@@ -95,8 +340,16 @@ export const Register = () => {
                 className={styles.input}
                 placeholder="Insira seu email"
                 required
-                {...register("email")}
+                autoComplete="username"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setErrors({
+                    ...errors,
+                    email: "",
+                  });
+                }}
               />
+              {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
             </div>
             <div style={{ textAlign: "center" }}>Segurança</div>
             <div className={styles.info}>
@@ -106,8 +359,17 @@ export const Register = () => {
                 autoComplete="current-password"
                 required
                 className={styles.input}
-                {...register("password")}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setErrors({
+                    ...errors,
+                    password: "",
+                  });
+                }}
               />
+              {errors.password && (
+                <p style={{ color: "red" }}>{errors.password}</p>
+              )}
             </div>
             <div className={styles.info}>
               <input
@@ -116,7 +378,17 @@ export const Register = () => {
                 autoComplete="current-password"
                 required
                 className={styles.input}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                  setErrors({
+                    ...errors,
+                    passwordConfirmation: "",
+                  });
+                }}
               />
+              {errors.passwordConfirmation && (
+                <p style={{ color: "red" }}>{errors.passwordConfirmation}</p>
+              )}
             </div>
             <div className={styles.declaration}>
               Declaro que sou maior de 18 anos e estou de acordo com a{" "}
@@ -127,7 +399,7 @@ export const Register = () => {
             </button>
           </form>
         </div>
-      </div>
+      </motion.div>
     </main>
   );
 };
