01c4f510c5da8b73e4293bf9caf39b46fd3c9c08
Isabella Lima <isabellal.prof@gmail.com>
2023-11-24 13:15:36 -0300

    ligação de jogos em desenvolvimento

--- src/pages/Aposta/Aposta.tsx
+++ src/pages/Aposta/Aposta.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import styles from "./Aposta.module.css";
 import { useEffect, useState } from "react";
-import {BuyCredits} from "components";
+import {BuyCredits, Register, Login} from "components";
 import user from "data/user.json";
+import { Link } from 'react-router-dom';
 
 
-
+interface Aposta {
+    onModalChange: (isOpen: boolean) => void;
+  }
+  
 
 export const Aposta = () => {
-    const [buyCredits, setBuyCredits] = useState(false);
 
+    const [buyCredits, setBuyCredits] = useState(false);
+    const [logado, setLogado] = useState(true);
 
     const toggleBuyCredits = () => {
         setBuyCredits(!buyCredits);
@@ -20,15 +25,17 @@ export const Aposta = () => {
       <div className={styles.contem}>
 
      <div className={styles.jogo}>
-      <iframe  width="100%" height="100%" src='https://www.ojogodosbichos.com/' ></iframe>
+      <iframe  width="100%" height="100%" src='https://api-test.salsagator.com/game?token=7f9d5928053fd92c797299e8d4a0e760&pn=doublex-staging&lang=pt&game=Candy&type=FREE' ></iframe>
      </div>
 
      <div className={styles.compra}>
        <div className={styles.inf}>
-       <h4 className={styles.saldo} onClick={toggleBuyCredits}>Saldo: {user?.winner_balance}</h4>
+       {logado === true ? (
+       <h4 className={styles.saldo} onClick={toggleBuyCredits}>Saldo:  {user?.winner_balance}</h4>
+       ) : null}
        {buyCredits ? <BuyCredits onModalChange={toggleBuyCredits} /> : null}
        <p className={styles.cedula} onClick={toggleBuyCredits}>.</p> 
-       <h4 className={styles.apst}>Aposta: 0,00</h4>
+       {/*<h4 className={styles.apst}>Aposta: 0,00</h4>*/}
        </div>
        
      </div>
