eea4ad89b6f515aef918dcb216a2b984b156374d
Pedro Neto <pedro.slveira@easychange.com.br>
2023-11-30 18:30:20 -0300

    Responsivo da IntroBar

--- src/components/IntroBar/IntroBar.tsx
+++ src/components/IntroBar/IntroBar.tsx
@@ -12,15 +12,19 @@ export const IntroBar = ({ title, paragraph, navigate }: IntroProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.innerIntro}>
-        <Link to={navigate} className={styles.arrow}>
-          <img src={Arrow} alt="" />
-          <div style={{ color: "#fff", fontSize: "15px" }}>voltar</div>
-        </Link>
-        <div className={styles.txt}>
-          <div className={styles.title}>{title}</div>
-          <div className={styles.paragraph}>{paragraph}</div>
+        <div className={styles.left}>
+          <Link to={navigate} className={styles.arrow}>
+            <img src={Arrow} alt="" />
+            <div style={{ color: "#fff", fontSize: "15px" }}>voltar</div>
+          </Link>
         </div>
-        <div className={styles.hidden}>oi</div>
+        <div className={styles.middle}>
+          <div className={styles.txt}>
+            <div className={styles.title}>{title}</div>
+            <div className={styles.paragraph}>{paragraph}</div>
+          </div>
+        </div>
+        <div className={styles.right}></div>
       </div>
     </div>
   );
