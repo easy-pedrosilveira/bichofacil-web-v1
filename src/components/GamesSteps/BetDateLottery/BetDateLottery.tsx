c7b86fcb4a20341f11035e0f8071bf6e210f5a7a
Pedro Neto <pedro.slveira@easychange.com.br>
2023-12-13 10:23:58 -0300

    Customizando input select

--- src/components/GamesSteps/BetDateLottery/BetDateLottery.tsx
+++ src/components/GamesSteps/BetDateLottery/BetDateLottery.tsx
@@ -1,9 +1,10 @@
 import styles from "./BetDateLottery.module.css";
 import { IModalities } from "interfaces";
 import { useEffect, useState } from "react";
-import Select from "react-select";
 import useGameContext from "data/hooks/useGameContext";
 import { format } from "date-fns";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
 
 interface BetDateLotteryProps {
   dateAndLottery: IModalities;
@@ -21,6 +22,7 @@ export const BetDateLottery = ({
   const [selectedDate, setSelectedDate] = useState<string>("");
   const [selectedDateValue, setSelectedDateValue] = useState<string>("");
   const [selectedLottery, setSelectedLottery] = useState<string[]>([]);
+  const animatedComponents = makeAnimated();
   const minDate = new Date();
   const maxDate = new Date();
   maxDate.setDate(maxDate.getDate() + 5);
@@ -60,6 +62,47 @@ export const BetDateLottery = ({
     dataLottery(selectedLottery);
   }, [selectedDate, selectedLottery]);
 
+  const customStyles = {
+    control: (provided: any, state: any) => ({
+      ...provided,
+      width: 270,
+      height: "auto",
+      paddingTop: 7,
+      paddingBottom: 7,
+      display: "flex",
+      justifyContent: "center",
+      borderRadius: 32,
+      border: state.isFocused ? "2px solid #3F73E3" : "1px solid #3F73E3",
+      background: "#FFF",
+      color: "#7A7786",
+      fontSize: 17,
+      textIndent: 10,
+      lineHeight: "normal",
+      flex: "1",
+    }),
+    menu: (provided: any) => ({
+      ...provided,
+      width: 230,
+    }),
+    option: (provided: any, state: any) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? "transparent" : "transparent",
+      color: state.isSelected ? "#FFF" : "#7A7786",
+      cursor: "pointer",
+    }),
+  };
+
+  const customTheme = (theme: any) => ({
+    ...theme,
+    colors: {
+      ...theme.colors,
+      primary: "#3F73E3",
+      primary75: "#3F73E3",
+      primary50: "#3F73E3",
+      primary25: "#3F73E3",
+    },
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.date}>
@@ -80,12 +123,19 @@ export const BetDateLottery = ({
         <div className={styles.title}>Loteria</div>
         <Select
           onChange={handleLotteryChange}
+          closeMenuOnSelect={false}
+          components={{ ...animatedComponents }}
           options={options}
           value={options.filter((option) =>
             selectedLottery.includes(option.value)
           )}
+          theme={customTheme}
           isMulti
           required
+          placeholder={
+            options.length > 0 ? options[0].label : "Selecione uma loteria"
+          }
+          styles={customStyles}
         />
       </div>
     </div>
